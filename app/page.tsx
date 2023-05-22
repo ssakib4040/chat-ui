787991e687e03a4383046c15908141e9d8258287
Sadman Sakib <ssakib4040@gmail.com>
2023-05-22 15:15:05 +0600

    basic structure added

---
+++ app/page.tsx
@@ -0,0 +1,133 @@
+export default function Home() {
+  return (
+    <div className="dev flex flex-col md:flex-row h-screen">
+      {/* left side */}
+      <div className="dev hidden lg:block md:w-[250px] w-full bg-[#f9f9f9]">
+        left
+      </div>
+
+      {/* main */}
+      <div className="dev flex-1 overflow-auto">
+        <div className="flex dev sticky top-0 p-3 bg-white">Main Header</div>
+
+        {/*  */}
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, us
+          repudiandae fuga sed quo error, incidunt non consectetur quasi. Animi,
+          odio veritatis assumenda corporis doloremque reprehenderit. Optio
+          perferendis quae, similique error laudantium iusto corrupti beatae
+          cumque id doloribus modi enim inventore incidunt, repudiandae repellat
+          itaque, consectetur illum reprehenderit accusantium iure ut nihil!
+          Aliquid assumenda dolore enim dolor quia voluptates optio nostrum ab.
+          Laudantium atque dignissimos culpa quae velit odit fuga. Illo
+          necessitatibus accusantium, eum magni fugiat placeat mollitia minima
+          vel sit. Quam explicabo, ullam rem voluptate dolor odio consectetur
+          numquam quisquam natus vel corporis et, nemo qui aperiam odit dolorum
+          voluptatum? Ex molestias similique libero. Officia harum aliquid
+          quaerat ullam, voluptatibus debitis perspiciatis vero consequatur enim
+          eveniet soluta placeat, dolorum odio asperiores hic. Non id quidem
+          ullam corrupti earum consequatur molestiae, molestias necessitatibus
+          deleniti culpa possimus fugit incidunt cum optio architecto ea!
+          Delectus, assumenda. Obcaecati aspernatur et commodi, ut ex eum,
+          soluta cum quae, fugit corrupti sapiente. Aspernatur ipsam autem,
+          suscipit reprehenderit pariatur dolorum quam quas voluptates, rem
+          amet, iste enim similique! Incidunt, tempore ratione assumenda
+          obcaecati consectetur quidem esse dignissimos neque deleniti adipisci
+          minus suscipit sint nostrum iusto repellendus delectus veritatis
+          cumque explicabo. Optio similique dignissimos architecto soluta,
+          tenetur perspiciatis illum fugiat delectus. Officia porro, odit
+          explicabo laborum illo harum sit tenetur. Ullam non doloribus vitae
+          obcaecati ducimus ipsam molestias dolore eos, eligendi praesentium
+          laboriosam earum eum, id laborum excepturi. Aliquid facilis aliquam
+          nihil. Repellendus, facilis dolore? In ea esse ex doloribus, cumque,
+          labore quidem blanditiis itaque amet commodi dignissimos, odit earum
+          corporis repellendus dolorem quisquam nam suscipit explicabo pariatur
+          fugiat tempore magnam. Quae delectus fugit numquam ad, blanditiis
+          repellat necessitatibus odio corporis est non animi beatae eaque nisi
+          libero. Pariatur cum quasi ipsa placeat recusandae rem architecto
+          labore ut nemo iure corrupti esse eligendi delectus, odit dolorem
+          consectetur rerum magnam molestias hic dicta optio obcaecati deserunt
+          dolore tenetur. Assumenda sed qui pariatur quasi vitae officiis
+          possimus odio non impedit quo. Cumque mollitia repellendus corporis
+          error nisi minus et, expedita vitae consectetur dolorum a sit fugiat
+          quam! Quaerat neque libero dolores praesentium, asperiores eligendi.
+          Iste provident repudiandae voluptatibus voluptas repellendus eos
+          animi, quos, incidunt officia velit dolorum voluptate delectus
+          doloribus blanditiis ipsa magni tempore labore cumque. Consequuntur
+          quasi numquam consectetur nobis exercitationem, error omnis similique
+          hic iusto minus incidunt deserunt! Accusantium illum magnam deleniti
+          eum ab quas tempore necessitatibus. Cum doloremque ab, corrupti eos
+          doloribus molestiae. Ea saepe sint id placeat aut suscipit laboriosam,
+          incidunt rem architecto natus minima magnam aperiam cupiditate unde
+          praesentium ad reprehenderit, sed excepturi ex, inventore voluptatibus
+          hic facilis! Possimus, fugit laudantium. Nisi minima architecto ipsum.
+          Impedit voluptatibus facere veritatis autem ut, quo velit minima
+          accusamus ratione nobis atque? Dolorum!
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, us
+          repudiandae fuga sed quo error, incidunt non consectetur quasi. Animi,
+          odio veritatis assumenda corporis doloremque reprehenderit. Optio
+          perferendis quae, similique error laudantium iusto corrupti beatae
+          cumque id doloribus modi enim inventore incidunt, repudiandae repellat
+          itaque, consectetur illum reprehenderit accusantium iure ut nihil!
+          Aliquid assumenda dolore enim dolor quia voluptates optio nostrum ab.
+          Laudantium atque dignissimos culpa quae velit odit fuga. Illo
+          necessitatibus accusantium, eum magni fugiat placeat mollitia minima
+          vel sit. Quam explicabo, ullam rem voluptate dolor odio consectetur
+          numquam quisquam natus vel corporis et, nemo qui aperiam odit dolorum
+          voluptatum? Ex molestias similique libero. Officia harum aliquid
+          quaerat ullam, voluptatibus debitis perspiciatis vero consequatur enim
+          eveniet soluta placeat, dolorum odio asperiores hic. Non id quidem
+          ullam corrupti earum consequatur molestiae, molestias necessitatibus
+          deleniti culpa possimus fugit incidunt cum optio architecto ea!
+          Delectus, assumenda. Obcaecati aspernatur et commodi, ut ex eum,
+          soluta cum quae, fugit corrupti sapiente. Aspernatur ipsam autem,
+          suscipit reprehenderit pariatur dolorum quam quas voluptates, rem
+          amet, iste enim similique! Incidunt, tempore ratione assumenda
+          obcaecati consectetur quidem esse dignissimos neque deleniti adipisci
+          minus suscipit sint nostrum iusto repellendus delectus veritatis
+          cumque explicabo. Optio similique dignissimos architecto soluta,
+          tenetur perspiciatis illum fugiat delectus. Officia porro, odit
+          explicabo laborum illo harum sit tenetur. Ullam non doloribus vitae
+          obcaecati ducimus ipsam molestias dolore eos, eligendi praesentium
+          laboriosam earum eum, id laborum excepturi. Aliquid facilis aliquam
+          nihil. Repellendus, facilis dolore? In ea esse ex doloribus, cumque,
+          labore quidem blanditiis itaque amet commodi dignissimos, odit earum
+          corporis repellendus dolorem quisquam nam suscipit explicabo pariatur
+          fugiat tempore magnam. Quae delectus fugit numquam ad, blanditiis
+          repellat necessitatibus odio corporis est non animi beatae eaque nisi
+          libero. Pariatur cum quasi ipsa placeat recusandae rem architecto
+          labore ut nemo iure corrupti esse eligendi delectus, odit dolorem
+          consectetur rerum magnam molestias hic dicta optio obcaecati deserunt
+          dolore tenetur. Assumenda sed qui pariatur quasi vitae officiis
+          possimus odio non impedit quo. Cumque mollitia repellendus corporis
+          error nisi minus et, expedita vitae consectetur dolorum a sit fugiat
+          quam! Quaerat neque libero dolores praesentium, asperiores eligendi.
+          Iste provident repudiandae voluptatibus voluptas repellendus eos
+          animi, quos, incidunt officia velit dolorum voluptate delectus
+          doloribus blanditiis ipsa magni tempore labore cumque. Consequuntur
+          quasi numquam consectetur nobis exercitationem, error omnis similique
+          hic iusto minus incidunt deserunt! Accusantium illum magnam deleniti
+          eum ab quas tempore necessitatibus. Cum doloremque ab, corrupti eos
+          doloribus molestiae. Ea saepe sint id placeat aut suscipit laboriosam,
+          incidunt rem architecto natus minima magnam aperiam cupiditate unde
+          praesentium ad reprehenderit, sed excepturi ex, inventore voluptatibus
+          hic facilis! Possimus, fugit laudantium. Nisi minima architecto ipsum.
+          Impedit voluptatibus facere veritatis autem ut, quo velit minima
+          accusamus ratione nobis atque? Dolorum!
+        </p>
+
+        <div className="flex dev sticky bottom-0 p-3 bg-white">Main Footer</div>
+
+        {/*  */}
+      </div>
+
+      {/* right side */}
+      <div className="dev hidden lg:block md:w-[250px] w-full bg-[#f9f9f9]">
+        right
+      </div>
+    </div>
+  );
+}
